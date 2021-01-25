b5d3a82a8ff1fcc60818c92b78128b991f7e6a8c
00Schnitzel <vogel2@uni-potsdam.de>
2021-01-25 19:20:35 +0100

    added CatergoryList

--- CategoryList.java
+++ CategoryList.java
@@ -2,8 +2,53 @@ public class CategoryList
 {
     private ArrayList<Category> categoryList;
 
-    CategoryList()
-    {
+    CategoryList() {
+        categroyList = new ArrayList<Category>
+    }
+
+    public void newCategroy(String name) {
+        Category temp = new Category(name);
+        categoryList.add(temp);
+    }
+
+    public void removeCategory(int index) {
+        Category temp = categoryList.get(index);
+        categoryList.remove(temp);
+    }
+
+    public void setCategory(int index, String name) {
+        Category temp = categoryList.get(index);
+        temp.setName(name);
+    }
+
+    public void sort() {
+
+    }
+
+    public void showList() {
         
+    }
+
+    public ArrayList getCategory(String search) {
+        ArrayList<Category> result =new ArrayList<Category>
+        search = search.subSequence(0,search.length-1);
+        for(int i = 0; i < categoryList.length()-1; i++) {
+            Category temp = categoryList.get(i);
+            if(temp.name.contains(search)) {
+                result.add(temp)
+            }
+        }
+        return result;
+    }
+
+    public boolean categoryInList(String search) {
+        search = search.subSequence(0,search.length-1);
+        for(int i = 0; i < categoryList.length()-1; i++) {
+            Category temp = categoryList.get(i);
+            if(temp.name.contains(search)) {
+                return True;
+            }
+        }
+        return False;
     }
 }
