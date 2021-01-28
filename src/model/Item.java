8cd67d78651bad527ce660466882a22a7b158532
mulrich20 <marculrich65@gmail.com>
2021-01-28 18:19:11 +0100

    package name to model

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,4 +1,4 @@
-package src.model;
+package model;
 
 public class Item {
     private Product product;
--- src/model/PurchaseList.java
+++ src/model/PurchaseList.java
@@ -1,6 +1,5 @@
-package src.model;
+package model;
 
-import src.model.Item;
 import java.util.ArrayList;
 
 public class PurchaseList {
