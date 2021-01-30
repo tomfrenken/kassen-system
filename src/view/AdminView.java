77cb035242587705e02822baaa45ff002ff31cdb
mulrich20 <marculrich65@gmail.com>
2021-01-30 21:24:00 +0100

    added views

---
+++ src/view/AdminView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class AdminView {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton neuesProduktButton;
+    private JButton produktLöschenButton;
+    private JButton kategorielisteAnzeigenButton;
+    private JButton button3;
+}
