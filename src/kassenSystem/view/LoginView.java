13cff9e6283d91d466498ea74db10a0889bcda9a
mulrich20 <marculrich65@gmail.com>
2021-01-30 22:44:33 +0100

    changed folder structure

--- src/kassenSystem/view/LoginView.java
+++ src/kassenSystem/view/LoginView.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 public class LoginView {
     private JButton verkäuferButton;
     private JButton adminButton;
+    private JPanel jp;
 
     public LoginView() {
         adminButton.addActionListener(new ActionListener() {
@@ -17,7 +18,7 @@ public class LoginView {
              */
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                jp.
             }
         });
         verkäuferButton.addActionListener(new ActionListener() {
@@ -31,5 +32,12 @@ public class LoginView {
 
             }
         });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Kassensystem");
+        frame.setContentPane(new LoginView().jp);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
