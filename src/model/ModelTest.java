425cdc1fa7dcb2cdc49e591aa2e4c4e4bd9b0415
tomfrenken <frenken.tom@gmail.com>
2021-01-30 06:20:04 +0100

    ignore .iml files, add some description to ModelTest

---
+++ src/model/ModelTest.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ModelTest {
+    public static void main(String[] args){
+        // This class is only for testing purpose during development.
+        // THe first thing we try is to instantiate every mode class to test their behaviour
+        // By doing so we build a temporary database, on which we can later test the controller and the view.
+        // Additionally, if we don't manage to write the database integration in time, we have this test file to
+        // show the functionality without a real database.
+    }
+}
