922f6e5f8b8e31b19a8599b3ded13a19d3325457
mulrich20 <marculrich65@gmail.com>
2021-01-30 22:05:54 +0100

    changed folder structure

---
+++ src/kassenSystem/controler/LoginViewController.java
@@ -0,0 +1,16 @@
+package kassenSystem.controler;
+
+import kassenSystem.view.LoginView;
+
+public class LoginViewController {
+    private LoginView view;
+
+    public LoginViewController() {
+        this.view = new LoginView();
+    }
+
+    public void updateView() {
+
+    }
+
+}
--- src/kassenSystem/model/Product.java
+++ src/kassenSystem/model/Product.java
@@ -1,4 +1,4 @@
-package model;
+package kassenSystem.model;
 
 public class Product {
     private String name;
