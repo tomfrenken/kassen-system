8c4396bac929d3fb6121d3c4c869d75895663092
mulrich20 <marculrich65@gmail.com>
2021-01-31 22:59:49 +0100

    change add in plist

--- src/kassenSystem/view/SellerView.java
+++ src/kassenSystem/view/SellerView.java
@@ -1,4 +1,21 @@
 package kassenSystem.view;
 
+import javax.swing.*;
+
 public class SellerView {
+    private JList list;
+    private JScrollPane scroll;
+    private JTextField suchfeldTextField;
+    private JButton hinzufuegenButton;
+    private JList list2;
+    private JScrollPane scroll2;
+    private JTextField anzahlTextField;
+    private JTextField suchfeldTextField1;
+    private JList list3;
+    private JScrollPane scroll3;
+    private JButton einkaufStornierenButton;
+    private JButton einkaufAbschließenButton;
+    private JPanel Zwischensumme;
+    private JButton artikelStornierenButton;
+    private JButton anpassenButton;
 }
