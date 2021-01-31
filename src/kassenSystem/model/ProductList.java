c7243a03b2e1bd583f3e80c95f8a34e0d873ac2c
mulrich20 <marculrich65@gmail.com>
2021-01-31 21:42:31 +0100

    change add in plist

--- src/kassenSystem/model/ProductList.java
+++ src/kassenSystem/model/ProductList.java
@@ -45,11 +45,9 @@ public class ProductList {
              }
              Product temp = new Product(name, id, stock, weight, weightUnit, price, category);
              productList.add(temp);
-             addToDatabase(temp);
         } else {
              Product temp = new Product(name, id, stock, weight, weightUnit, price, category);
              productList.add(temp);
-             addToDatabase(temp);
         }
     }
     /**
