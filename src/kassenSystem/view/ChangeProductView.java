0ddc1946016193e7c782b55d7767daaa16481aed
mulrich20 <marculrich65@gmail.com>
2021-01-31 22:27:39 +0100

    change add in plist

--- src/kassenSystem/view/ChangeProductView.java
+++ src/kassenSystem/view/ChangeProductView.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class ChangeProductView {
     private JTextField textField1;
-    private JButton button1;
+    private JButton aktualisierenButton;
     private JTextField textField2;
     private JComboBox comboBox1;
     private JTextField textField3;
---
+++ src/kassenSystem/view/addProductView.java
@@ -0,0 +1,14 @@
+package kassenSystem.view;
+
+import javax.swing.*;
+
+public class addProductView {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField5;
+    private JComboBox comboBox2;
+    private JTextField textField4;
+    private JTextField textField3;
+    private JComboBox comboBox1;
+    private JButton bestaetigenButton;
+}
