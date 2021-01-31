2494033f9f11821ec12c6f868dce11017107cc57
mulrich20 <marculrich65@gmail.com>
2021-01-31 22:41:49 +0100

    change add in plist

--- src/kassenSystem/view/CategoryListView.java
+++ src/kassenSystem/view/CategoryListView.java
@@ -1,4 +1,12 @@
 package kassenSystem.view;
 
+import javax.swing.*;
+
 public class CategoryListView {
+    private JButton hinzufuegen;
+    private JButton anpassenButton;
+    private JButton loeschenButton;
+    private JTextField suchfeldTextField;
+    private JList list;
+    private JScrollPane scroll;
 }
