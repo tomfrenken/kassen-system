462eeb87457526bddaba7b0a0094f1ddc8cfa51c
mulrich20 <marculrich65@gmail.com>
2021-01-31 22:16:11 +0100

    change add in plist

--- src/kassenSystem/view/AdminView.java
+++ src/kassenSystem/view/AdminView.java
@@ -15,8 +15,8 @@ public class AdminView extends JFrame {
     private JComboBox comboBox1;
     
     private JPanel jp;
-    private JTextArea TextArea;
-    private JScrollPane ScrollPane;
+    private JTextArea ta;
+    private JScrollPane sp;
 
     public AdminView(String name) {
         super(name);
