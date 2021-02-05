e2853fc5291cd82b957a643e3e013ff2654befdb
mulrich20 <marculrich65@gmail.com>
2021-02-05 20:38:27 +0100

    AdminLogin

---
+++ src/kassenSystem/view/AdminLoginView.java
@@ -0,0 +1,9 @@
+package kassenSystem.view;
+
+import javax.swing.*;
+
+public class AdminLoginView {
+    private JPasswordField passwordField1;
+    private JPanel panel1;
+    private JButton loginButton;
+}
